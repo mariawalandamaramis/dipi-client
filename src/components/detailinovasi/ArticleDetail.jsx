773bc1b2c0b6ed866f8aa85dc7b7aa2fa65e3091
listianike21@gmail.com <listianike21@gmail.com>
2024-01-21 13:03:34 +0700

    componenarticle Detail + tab belum selesai

--- src/components/detailinovasi/ArticleDetail.jsx
+++ src/components/detailinovasi/ArticleDetail.jsx
@@ -1,68 +1,54 @@
 import React, {Component} from 'react';
 import { useState } from "react";
-const ArticleDetail = ({activeKey, children}) => {
-  const [key, setkey] = useState(activeKey)
+import '../../App.css';
 
-  return (
-  <div class="max-w-2xl mx-auto">
-    
-    <div class="border-b border-gray-200 dark:border-gray-700 mb-4">
-        <ul class="flex flex-wrap -mb-px" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
-            <li class="mr-2" role="presentation">
-                <button class="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300" id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">
-                {children.map(item => {
-            return (
-              <span key={item.props.aKey} className={key === item.props.akey ? "tab-item active" : "tab-item"}
-              onClick={() => setKey(item.props.aKey)}>
-                {item.props.title}
-              </span>
-            )
-          })}
+function ArticleDetail(){
+  const [state, setState]= useState(1);
 
-                </button>
-            </li>
-            <li class="mr-2" role="presentation">
-                <button class="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300 active" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="true">Dashboard</button>
-            </li>
-            <li class="mr-2" role="presentation">
-                <button class="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300" id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Settings</button>
-            </li>
-            <li role="presentation">
-                <button class="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300" id="contacts-tab" data-tabs-target="#contacts" type="button" role="tab" aria-controls="contacts" aria-selected="false">Contacts</button>
-            </li>
-        </ul>
-    </div>
-    <div id="myTabContent">
-        <div class="bg-gray-50 p-4 rounded-lg dark:bg-gray-800 hidden" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-        {children.map(item => {
-            return (
-              <div key={item.props.aKey} className={key === item.props.akey ? "tab-pane active" : "tab-item"}>
-                <h2>{item.props.title}</h2>
-                <span>{item.props.children}</span>
-              </div>
-            )
-          })}
+  const action = (index) => {
+  
+
+  setState(index)
+
+  }
+    return(
+        <>
+        <div className='box'>
+            <div className='tabs'>
+                <div  onClick={()=>action(1)}className={`${state===1 ? 'tab active-tab':'tab'}`}>
+                       
+                </div>
+
+                <div className={`${state===2 ? 'tab active-tab':'tab'}`} onClick={()=>action(2)}>
+                    tab2
+                </div>
+                
+                <div className={`${state===3 ? 'tab active-tab':'tab'}`} onClick={()=>action(3)}>
+                    tab3 
+                </div>
+            </div>
+
+            <div className="contents">
+                <div  className="content active-content">
+                <h2>Content 1</h2>
+                <p>this is Contente 1</p>
+                </div>
+
+                <div className="contents">
+                <h2>Content 2</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam dolores assumenda excepturi adipisci culpa dignissimos! Nobis, vel ab. Harum nesciunt debitis modi, impedit et distinctio fuga eos voluptate consequuntur quis.this is2</p>
+                </div>
+
+                <div  className="contents">
+                <h2>Content 3</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit corrupti sit vero beatae, dolorem fuga, numquam voluptatibus est similique tempora ad. Laudantium facere alias repellat rem impedit minus, placeat ab!</p>
+                </div>
+            </div>
         </div>
-        <div class="bg-gray-50 p-4 rounded-lg dark:bg-gray-800 hidden" id="contacts" role="tabpanel" aria-labelledby="contacts-tab">
-            <span class="text-gray-500 dark:text-gray-400 text-sm"> {children.map(item => {
-            return (
-              <div key={item.props.aKey} className={key === item.props.akey ? "tab-pane active" : "tab-item"}>
-                <h2>{item.props.title}</h2>
-                <span>{item.props.children}</span>
-              </div>
-            )
-          })}</span>
-        </div>
-    </div>
+        </>
+    )
 
-    
-</div>
-
-      )
 }
-
-export const Tab = () => {
-  return
-}
-
 export default ArticleDetail
+
+
--- src/pages/DetailPageInovasi.jsx
+++ src/pages/DetailPageInovasi.jsx
@@ -2,13 +2,8 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import DetailHeader from '../components/detailinovasi/DetailHeader'
-import ArticleDetail, {Tab} from '../components/detailinovasi/ArticleDetail'
+import ArticleDetail from '../components/detailinovasi/ArticleDetail'
 
-const tab = [
-  {aKey: "article1", title: "Detail", content: "Reactaksdakdsahdhaksjhdaksd"  },
-  {aKey: "article2", title: "Kabar Baru", content: "Reactaksdakdsahdhaksjhdaksd"  },
-  {aKey: "article3", title: "Tanya Jawab", content: "Reactaksdakdsahdhaksjhdaksd"  }
-]
 
 
 const DetailPageInovasi = () => {
@@ -16,9 +11,7 @@ const DetailPageInovasi = () => {
       <>
         <Navbar />
         <DetailHeader />
-        <ArticleDetail activeKey="article1">
-          {tab.map(item => <Tab key={item.aKey} aKey={item.aKey} title={item.title}>{item.content}</Tab>)}
-        </ArticleDetail>
+        <ArticleDetail />
         <Footer />
       </>
     )
