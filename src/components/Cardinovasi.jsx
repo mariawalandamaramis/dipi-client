27f480ce20124f9c3c3e4246b1c546328a693942
fidaatag <fidaa2fidaa@gmail.com>
2024-02-06 15:42:09 +0700

    mapping respon landing, list, detail, dukungan sudh sesuai API get, tinggal ganti API asli

--- src/components/Cardinovasi.jsx
+++ src/components/Cardinovasi.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Cardinovasi = ({ addLocation, addButton, linkButton, handleButton, image, category, title, time,id }) => {
+const Cardinovasi = ({ addLocation, addButton, linkButton, handleButton, image,
+  catagory_id, user_name, inovation_name, time, id, persenTarget, kota, propinsi }) => {
   return (
     <>
       <div className='border-2 rounded-xl'>
-        <img className='object-cover h-40 w-full rounded-t-xl' src={image} alt="" srcset="" />
+        <img className='object-cover h-40 w-full rounded-t-xl' src={image || '/BlankData.jpg'} alt="" srcset="" />
         <div className='flex flex-col gap-8 px-6 py-5'>
           <div className='flex flex-col gap-2 h-32'>
             <Link to={`/inovasi/${id}`}>
-              <p className='text-base font-thin underline'>{category}</p>
-              <h2 className='text-lg font-extrabold line-clamp-2'>{title}</h2>
-              <p className='text-base font-thin'>Oleh: Tralala Lulu</p>
+              <p className='text-base font-thin underline'>{catagory_id}</p>
+              <h2 className='text-lg font-extrabold line-clamp-2'>{inovation_name}</h2>
+              <p className='text-base font-thin'>Oleh: {user_name}</p>
             </Link>
           </div>
-          <div className='flex flex-col gap-2 border-t-2'>
-            <p className='text-base font-thin'><span className='font-semibold'>50%</span> pendanaan sudah tercapai</p>
+          <div className='flex flex-col gap-2 '>
+            <div className="w-full bg-slate-300 rounded-lg overflow-hidden">
+              <div className="bg-green-900 h-1" style={{ width: `${persenTarget}%` }}></div>
+            </div>
+            <p className='text-base font-thin'><span className='font-semibold'>{persenTarget}%</span> pendanaan sudah tercapai</p>
             <p className='text-base font-semibold'>{time} Hari lagi</p>
           </div>
           {addButton ? (
@@ -33,7 +37,7 @@ const Cardinovasi = ({ addLocation, addButton, linkButton, handleButton, image, 
         {addLocation ? (
           <div className='flex bg-green-900 text-white gap-1 px-6 py-3 rounded-b-xl'>
             <img src="PinMap.svg" alt="" srcset="" />
-            <p className='text-base font-normal'>Palopo, Sulawesi Selatan</p>
+            <p className='text-base font-normal line-clamp-2 xl:line-clamp-1'>{kota}, {propinsi}</p>
           </div>
         ) : (<div></div>)}
       </div>
--- src/components/detailinovasi/KabarbaruDesc.jsx
+++ src/components/detailinovasi/KabarbaruDesc.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const KabarbaruDesc = ({ artikelKe, name, description }) => {
+const KabarbaruDesc = ({ artikelKe, foto, name, created, title, description }) => {
 
   // sampel contoh text langsung dari editor text
   // const resultAPiTEXT = "<p>Indonesia merupakan negara tropis yang memiliki banyak pohon kelapa. Setiap tahun, Indonesia menghasilkan sekitar 31,4 juta ton buah kelapa. Batok kelapa merupakan limbah dari pengolahan buah kelapa yang biasanya dibuang begitu saja atau digunakan sebagai bahan bakar.</p><p><br></p><p>Batok kelapa memiliki sifat yang keras dan kuat. Sifat ini menjadikan batok kelapa sebagai bahan yang cocok untuk digunakan sebagai pelindung kepala. Selain itu, batok kelapa juga merupakan bahan yang ramah lingkungan dan dapat didaur ulang.</p><p><br></p><p>Berdasarkan fakta dan potensi batok kelapa, muncul ide untuk mengembangkan helm berbahan dasar batok kelapa. Helm ini diharapkan dapat menjadi solusi untuk permasalahan limbah batok kelapa sekaligus memberikan perlindungan kepala yang aman dan ramah lingkungan.</p>"
@@ -9,14 +9,14 @@ const KabarbaruDesc = ({ artikelKe, name, description }) => {
   return (
     <div className="flex flex-col gap-5 border border-green-900 rounded-lg py-10 px-8">
       <p className="text-2xl font-normal">{`Update #${artikelKe}`}</p>
-      <h4 className="text-3xl font-bold">50 % Dana Dukungan Untuk Mulai Produksi</h4>
+      <h4 className="text-3xl font-bold">{title}</h4>
       <div className='flex items-center gap-2'>
         <div className='w-10 md:w-10 h-10 md:h-10'>
-          <img className='w-full h-full rounded-full object-cover' src="/Hero.png" alt="" srcset="" />
+          <img className='w-full h-full rounded-full object-cover' src={foto} alt="" srcset="" />
         </div>
         <div>
           <p className='text-base font-semibold'>{name}</p>
-          <p className="text-xs font-extralight">20 Januari 2040</p>
+          <p className="text-xs font-extralight">{created}</p>
         </div>
       </div>
       <span className="w-full border"></span>
